91bedea47e056175757838f4f2c86412e35fb590
sir-wallaby <wesley.taylor303@gmail.com>
2017-01-31 19:30:22 -0500

    Lots of changes to use the new "mode" structure from the API (json calls)

--- Program.cs
+++ Program.cs
@@ -12,11 +12,13 @@ namespace Discord01
     {
         static void Main(string[] args)
         {
-            new Program().Start();
+            //invoke start method
+            new Program().Start(); 
         }
 
         private DiscordClient _client;
 
+        //start method - includes most areas needed to create the discordbot
         public void Start()
         {
 
@@ -24,12 +26,12 @@ namespace Discord01
             {
                 //use this url with the bots client ID to add to server. 
                 //https://discordapp.com/oauth2/authorize?client_id=268541960086749185&scope=bot&permissions=0
-
-                x.AppName = "Discord Test Bot"; //actual bot name
-                x.AppUrl = ""; //what is this exactly? how does it connect
+                //actual bot name
+                x.AppName = "Discord Test Bot";
+                //github
+                x.AppUrl = "https://github.com/sir-wallaby/Discord01"; 
                 x.LogLevel = LogSeverity.Info;
-                x.LogHandler = Log;
-               
+                x.LogHandler = Log;             
 
             });
 
@@ -57,11 +59,12 @@ namespace Discord01
         {
             var cService = _client.GetService<CommandService>();
 
+            //testing the create Command method from discord.net
             cService.CreateCommand("ping")
-                .Description("Returns some random shit")
+                .Description("Returns Pong")
                 .Do(async (e) =>
                 {
-                    await e.Channel.SendMessage("I'm a bot and you like dicks");
+                    await e.Channel.SendMessage("Pong");
                 });
 
             cService.CreateCommand("hello")
@@ -94,25 +97,29 @@ namespace Discord01
             //basic code for the agora.gg website retrieve elo based on parameter.
             //endpoint to see if a user exists: https://api.agora.gg/players/search/wallaby32
             //then use this to call the api to get stats: https://api.agora.gg/players/921041
-            //Maybe create another elo command that is able to search on psnID?
             cService.CreateCommand("elo")
                 .Description("this will retrieve the paragon elo for specified user. Usage !elo username")
                 .Parameter("user", Discord.Commands.ParameterType.Unparsed)
                 
                 .Do(async (e) =>
-                {
-                    
+                {                    
                     var userName = e.GetArg("user"); //you can use spaces so removed the replace method.                    
-                    var isValid= $"https://api.agora.gg/players/search/{userName}";
-
+                    var searchValue= $"https://api.agora.gg/players/search/{userName}";
+                    
+                    //open webclient
                     WebClient client = new WebClient();
-                    string url = client.DownloadString(isValid);
-                    client.Dispose();
-
-                    dynamic agoraData = JObject.Parse(url);
-                    int agoraPlayerId = agoraData.data[0].id;                    
-                    string name = agoraData.data[0].name;                                     
-                    //account for pc names
+                    
+                    //download string into url variable
+                    string url = client.DownloadString(searchValue);
+                    client.Dispose();                   
+                    
+                    //parsing Json Data from downloaded url
+                    dynamic agoraData = JObject.Parse(url);                
+                    int agoraPlayerId = agoraData.data[0].id;                                        
+                    string name = agoraData.data[0].name;
+                                                         
+                    //logic for pc names versus ps4 names.
+                    //note: will default to PC name if they are different names, including cases.
                     if (String.IsNullOrEmpty(name))
                     {
                         agoraPlayerId = agoraData.data[1].id;
@@ -120,58 +127,102 @@ namespace Discord01
                     else
                     {
                         agoraPlayerId = agoraData.data[0].id;
-                    }
-
-                    //Console.WriteLine(name.ToString());
-                    //Console.WriteLine(agoraPlayerId.ToString());
-
+                    }                     
+                   
                     //using player id grab stats from api
                     var playerStatsPage = $"https://api.agora.gg/players/{agoraPlayerId}";
+                    
+                    //new web client
                     WebClient statsClient = new WebClient();
+                    
+                    //download json data as a string into statsURL variable
                     string statsUrl = client.DownloadString(playerStatsPage);
                     client.Dispose();
 
-                    dynamic agoraPlayerStatsData = JObject.Parse(statsUrl);
+                    //parse json data from statsurl
+                    var agoraPlayerStatsData = JObject.Parse(statsUrl);
+                    int numberOfStatItems = agoraPlayerStatsData.SelectToken("data.stats").Count();
+                    var pvpData = agoraPlayerStatsData.SelectTokens("data.stats[*].mode");
+                    var multiStatData = agoraPlayerStatsData.SelectTokens("data.stats[*]");
+                    string multiStatDataDeserialed = JsonConvert.SerializeObject(multiStatData);                   
                     
-                    //create and initialize gameModeSignal array
-                    int[] gameModeSignal = new int[3];
-                    //assign all stats values to the array
-                    gameModeSignal[0] = agoraPlayerStatsData.data.stats[0].mode;
-                    gameModeSignal[1] = agoraPlayerStatsData.data.stats[1].mode; //still a bug here. will have to fix 
-                    //if (String.IsNullOrEmpty(agoraPlayerStatsData.data.stats[2].mode))
-                    //{
-                    //    return;
-                    //}
-                    //else
-                    //{
-                    //    gameModeSignal[2] = agoraPlayerStatsData.data.stats[2].mode;
-                    //}
-                   
+                    //second attempt
+                    dynamic agoraPlayerStatsDataDynamic = JObject.Parse(statsUrl);
 
-                    //value that I want to search for. I'm assuming 4 is the MP mode
-                    int myValue = 4; 
-                    //get the index back, which, tells me which stats array part I need to look in for correct stats.
-                    int arrayIndex = Array.IndexOf(gameModeSignal, myValue);
+                    //if statement variables
+                    dynamic agoraSingleItemStats = 0;
+                    dynamic multipleValuesFromStats0 = 0;
+                    dynamic multipleValuesFromStats1 = 0;
+                    dynamic multipleValuesFromStats2 = 0;
+                    dynamic mutipleOutput0 = 0;
+                    dynamic mutipleOutput1 = 0;
+                    dynamic finalPlayerStats = 0;
 
-                    //for (int i = 0; i <= gameModeSignal.Length;i++)
-                    //{
-                    //    Console.WriteLine(gameModeSignal[i]);
-                    //}
+                    //if structure to help determine which stats to use.
+                    if (numberOfStatItems == 1)
+                    {
+                        agoraSingleItemStats = agoraPlayerStatsDataDynamic.data.stats[0];
+                        finalPlayerStats = agoraSingleItemStats;           
+                    }                   
 
-                    //Console.WriteLine(arrayIndex);
+
+                    if (numberOfStatItems == 2)
+                    {
+                        multipleValuesFromStats0 = agoraPlayerStatsDataDynamic.data.stats[0];
+                        multipleValuesFromStats1 = agoraPlayerStatsDataDynamic.data.stats[1];
+
+                        string modeValue0 = multipleValuesFromStats0.mode;
+                        string modeValue1 = multipleValuesFromStats1.mode;
+
+                        if (modeValue0 == "4")
+                        {
+                            mutipleOutput0 = multipleValuesFromStats0;                         
+                        }
+                        if (modeValue1 == "4")
+                        {
+                            mutipleOutput0 = multipleValuesFromStats1;                            
+                        }
+
+                        finalPlayerStats = mutipleOutput0;
+                    }
+
+                    if (numberOfStatItems == 3)
+                    {
+                        multipleValuesFromStats0 = agoraPlayerStatsDataDynamic.data.stats[0];
+                        multipleValuesFromStats1 = agoraPlayerStatsDataDynamic.data.stats[1];
+                        multipleValuesFromStats2 = agoraPlayerStatsDataDynamic.data.stats[2];
+
+                        string modeValue0 = multipleValuesFromStats0.mode;
+                        string modeValue1 = multipleValuesFromStats1.mode;
+                        string modeValue2 = multipleValuesFromStats2.mode;
+
+                        if (modeValue0 == "4")
+                        {
+                            mutipleOutput1 = multipleValuesFromStats0;
+                        }
+                        if (modeValue1 == "4")
+                        {
+                            mutipleOutput1 = multipleValuesFromStats1;
+                        }
+                        if (modeValue2 == "4")
+                        {
+                            mutipleOutput1 = multipleValuesFromStats2;
+                        }
+
+                        finalPlayerStats = mutipleOutput1;
+
+                    }           
                     
                     //store the actual elo
-                    decimal elo = agoraPlayerStatsData.data.stats[arrayIndex].elo;
-                    //store wins/losses/etc for other stats
-                    double wins = agoraPlayerStatsData.data.stats[arrayIndex].wins;
-                    double gamesplayed = agoraPlayerStatsData.data.stats[arrayIndex].gamesPlayed;
+                    decimal elo = finalPlayerStats.elo;
+                    //store wins/ losses / etc for other stats
+   
+                     double wins = finalPlayerStats.wins;
+                    double gamesplayed = finalPlayerStats.gamesPlayed;
                     double winPercentage = (wins / gamesplayed);
-                    double kills = agoraPlayerStatsData.data.stats[arrayIndex].kills; double deaths = agoraPlayerStatsData.data.stats[arrayIndex].deaths; double assists = agoraPlayerStatsData.data.stats[arrayIndex].assists;
+                    double kills = finalPlayerStats.kills; double deaths = finalPlayerStats.deaths; double assists = finalPlayerStats.assists;
                     double kd = (kills + assists);
-                    double kda = (kd / deaths);
-                    //Console.WriteLine(elo);
-                    //Console.WriteLine(wins);
-                    //Console.WriteLine(agoraPlayerStatsData.data.stats[0].elo);
+                    double kda = (kd / deaths);                   
 
                     //await e.Channel.SendMessage("");
                     await e.Channel.SendMessage("Agora Elo: " + elo.ToString() + "\n" +
